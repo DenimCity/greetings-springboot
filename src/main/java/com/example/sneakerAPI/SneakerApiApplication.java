b86bbef0d70e9640726eada628c408ec1f25cdf1
DenimCity <altidorj1@gmail.com>
2020-01-13 08:34:30 -0500

    initial spring boot api

---
+++ src/main/java/com/example/sneakerAPI/SneakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sneakerAPI/SneakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
